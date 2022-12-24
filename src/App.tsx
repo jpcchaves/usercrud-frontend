4ce37b33f2780e4cfbbe5b858dbfdd50eda04f83
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-12-24 15:19:24 -0300

    feat: add navbar and folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./layout/Navbar";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
