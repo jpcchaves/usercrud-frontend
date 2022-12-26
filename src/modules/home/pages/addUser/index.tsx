abc295c575086480665624510b19a61cf442b23e
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-12-25 21:55:29 -0300

    feat: create formik and add form validations

--- src/modules/home/pages/addUser/index.tsx
+++ src/modules/home/pages/addUser/index.tsx
@@ -1,6 +1,8 @@
+import { useFormik } from "formik";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useApi } from "../../../../hooks/useApi";
+import { UserValidation } from "../../../../validation/UserValidation";
 import AddUserView from "./view";
 
 const AddUser = () => {
@@ -8,44 +10,36 @@ const AddUser = () => {
 
   const [error, setError] = useState([]);
 
-  const [user, setUser] = useState({
-    name: "",
-    username: "",
-    email: "",
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      name: "",
+      username: "",
+      email: "",
+    },
+    validationSchema: UserValidation,
+    onSubmit: async (values) => {
+      try {
+        await useApi.post("/user", values);
+
+        navigate("/");
+      } catch (error: any) {
+        if (error.response) {
+          const { data } = error.response;
+          setError(Object.values(data));
+        }
+        console.log(error.message);
+      }
+    },
   });
 
-  const { name, username, email } = user;
-
-  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUser({ ...user, [e.target.name]: e.target.value });
-  };
+  console.log(formik);
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    try {
-      await useApi.post("/user", user);
-
-      navigate("/");
-    } catch (error: any) {
-      if (error.response) {
-        const { data } = error.response;
-        setError(Object.values(data));
-      }
-      console.log(error.message);
-    }
   };
 
-  return (
-    <AddUserView
-      onSubmit={onSubmit}
-      name={name}
-      username={username}
-      email={email}
-      onInputChange={onInputChange}
-      error={error}
-    />
-  );
+  return <AddUserView onSubmit={onSubmit} error={error} formik={formik} />;
 };
 
 export default AddUser;
--- src/modules/home/pages/addUser/view.tsx
+++ src/modules/home/pages/addUser/view.tsx
@@ -1,80 +1,129 @@
 import { Link } from "react-router-dom";
-import { User } from "../../../../types/User";
+import { Formik, Form } from "formik";
+import { Input } from "reactstrap";
+import { FormikHelpers, FormikValues } from "formik/dist/types";
+import { FormFeedback } from "reactstrap";
 
-interface AddUserProps extends User {
+interface AddUserProps {
   onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-  onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   error: any;
+  formik: FormikValues;
 }
 
-const AddUserView = ({
-  onSubmit,
-  name,
-  username,
-  email,
-  onInputChange,
-  error,
-}: AddUserProps) => {
+const AddUserView = ({ error, formik }: AddUserProps) => {
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
           <h2 className="text-center m-4">Cadastrar Usuário</h2>
-          <form onSubmit={(e) => onSubmit(e)}>
-            <div className="mb-3">
-              <label htmlFor="name" className="form-label">
-                Nome
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                name="name"
-                value={name}
-                placeholder="Digite seu nome..."
-                onChange={(e) => onInputChange(e)}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="username" className="form-label">
-                Usuário
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                name="username"
-                value={username}
-                placeholder="Digite seu usuário..."
-                onChange={(e) => onInputChange(e)}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="email" className="form-label">
-                Email
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                name="email"
-                value={email}
-                placeholder="Digite seu email..."
-                onChange={(e) => onInputChange(e)}
-              />
-            </div>
-            {error &&
-              error.map((err: string, idx: number) => (
-                <div key={idx} className="d-flex align-items-center">
-                  <p className="text-danger">{err}</p>
-                </div>
-              ))}
-            <div className="d-flex justify-content-end">
-              <button type="submit" className="btn btn-outline-primary">
-                Criar
-              </button>
-              <Link to="/" className="btn btn-outline-danger mx-2">
-                Cancelar
-              </Link>
-            </div>
-          </form>
+          <Formik
+            initialValues={{
+              name: "",
+              username: "",
+              email: "",
+            }}
+            onSubmit={function (
+              values: { name: string; username: string; email: string },
+              formikHelpers: FormikHelpers<{
+                name: string;
+                username: string;
+                email: string;
+              }>
+            ): void | Promise<any> {
+              throw new Error("Function not implemented.");
+            }}
+          >
+            <Form
+              onSubmit={(e) => {
+                e.preventDefault();
+                formik.handleSubmit();
+                return false;
+              }}
+            >
+              <div className="mb-3">
+                <label htmlFor="name" className="form-label">
+                  Nome
+                </label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  name="name"
+                  placeholder="Digite seu nome..."
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.name || ""}
+                  invalid={
+                    formik.touched.name && formik.errors.name ? true : false
+                  }
+                />
+                {formik.touched.name && formik.errors.name ? (
+                  <FormFeedback type="invalid">
+                    {formik.errors.name}
+                  </FormFeedback>
+                ) : null}
+              </div>
+              <div className="mb-3">
+                <label htmlFor="username" className="form-label">
+                  Usuário
+                </label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  name="username"
+                  placeholder="Digite seu usuário..."
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.username || ""}
+                  invalid={
+                    formik.touched.username && formik.errors.username
+                      ? true
+                      : false
+                  }
+                />
+                {formik.touched.username && formik.errors.username ? (
+                  <FormFeedback type="invalid">
+                    {formik.errors.username}
+                  </FormFeedback>
+                ) : null}
+              </div>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  name="email"
+                  placeholder="Digite seu email..."
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email || ""}
+                  invalid={
+                    formik.touched.email && formik.errors.email ? true : false
+                  }
+                />
+                {formik.touched.email && formik.errors.email ? (
+                  <FormFeedback type="invalid">
+                    {formik.errors.email}
+                  </FormFeedback>
+                ) : null}
+              </div>
+              {error &&
+                error.map((err: string, idx: number) => (
+                  <div key={idx} className="d-flex align-items-center">
+                    <p className="text-danger">{err}</p>
+                  </div>
+                ))}
+              <div className="d-flex justify-content-end">
+                <button type="submit" className="btn btn-outline-primary">
+                  Criar
+                </button>
+                <Link to="/" className="btn btn-outline-danger mx-2">
+                  Cancelar
+                </Link>
+              </div>
+            </Form>
+          </Formik>
         </div>
       </div>
     </div>
